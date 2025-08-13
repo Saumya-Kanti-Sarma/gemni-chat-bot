19b8a46a794fbaee5ea0e30641e54a92cd5f01f4
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-08-13 19:48:25 +0530

    added basics of gemni

---
+++ basic.py
@@ -0,0 +1,29 @@
+'''
+This is the basic use of gemni to crreate a simple chatbot with menory
+'''
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+KEY = os.getenv("GOOGLE_API_KEY")
+
+client = genai.Client(api_key=KEY)
+
+response = client.models.generate_content(
+  model="gemini-2.0-flash-lite-001",
+  contents="make a simple hello world pogramme in python"
+)
+print(response.text)
+#response:
+'''
+```python
+print("Hello, world!")
+```
+This single line of code does the following:
+
+1.  `print()` is a built-in Python function that displays output.
+2.  `"Hello, world!"` is a string literal, which is the text you want to display.
+
+When you run this code, it will print "Hello, world!" to your console.
+'''
