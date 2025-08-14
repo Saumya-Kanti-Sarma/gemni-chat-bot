6ced03030d64ee943af9801b2b8aebfe414b2b17
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-08-14 21:31:37 +0530

    added getData fuunction

--- chatbot/model_2.py
+++ chatbot/model_2.py
@@ -6,6 +6,7 @@ This is an advance chatbot for an ecommerce website that sells shoes. It has too
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 KEY = os.getenv("GOOGLE_API_KEY")
@@ -15,6 +16,19 @@ client = OpenAI(api_key=KEY,base_url="https://generativelanguage.googleapis.com/
 chat_history = []
 chat_history.append({"role": "system", "content": "You are Tulio, a supportive yet straightforward AI assistant for customers of a e commerce website called ALL Time SHoes. Your main goal is to support customer queries and show them product according to their. You do not answer questions outside these topics."})
 
+
+# functions
+def getData(category,brand,minPrice,maxPrice,page):
+  base_url = "http://localhost:5000/shoes"
+  params = []
+  if(category): params.append(f"category={category}")
+  elif(brand): params.append(f"brand={brand}")
+  elif(minPrice): params.append(f"minPrice={minPrice}")
+  elif(maxPrice): params.append(f"maxPrice={maxPrice}")
+  elif(page): params.append(f"page={page}")
+  url = base_url + "?" + f"{"&".join(params)}"
+  response = requests.get(url=url)
+  return response.content
 
 
 while True:
