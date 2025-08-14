06ad744bf6d27013300e20550e1129d1438fc782
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-08-14 21:10:59 +0530

    made basic chat-bot

---
+++ chatbot/model_2.py
@@ -0,0 +1,38 @@
+"""
+Model No:2
+This is an advance chatbot for an ecommerce website that sells shoes. It has tool calling ability and chat history
+"""
+
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+KEY = os.getenv("GOOGLE_API_KEY")
+
+client = OpenAI(api_key=KEY,base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
+
+chat_history = []
+chat_history.append({"role": "system", "content": "You are Tulio, a supportive yet straightforward AI assistant for customers of a e commerce website called ALL Time SHoes. Your main goal is to support customer queries and show them product according to their. You do not answer questions outside these topics."})
+
+
+
+while True:
+  user_input = input("You: ")
+
+  if(user_input == "bye" ):
+    chat_history.clear()
+    break
+
+  message = {"role":"user", "content":user_input}
+  chat_history.append(message)
+
+  response = client.chat.completions.create(
+    model="gemini-2.0-flash-lite-001",
+    messages=chat_history,
+    max_tokens=500
+  )
+  chat_history.append({"role":"assistant", "content":response.choices[0].message.content})
+  print(response.choices[0].message.content)
+  if(len(chat_history) >=10):
+    chat_history.remove(chat_history[1])
